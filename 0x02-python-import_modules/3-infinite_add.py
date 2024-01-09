3c0db5debca104cf2cdf353a7398589aa1f08847
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-01-09 18:42:42 +0000

    debugging task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
+import sys
+argv = sys.argv[1:]
 
-    result = 0
-    for arg in argv:
-        result += int(arg)
-    print(result)
+result = 0
+for arg in argv:
+    result += int(arg)
+print(result)
