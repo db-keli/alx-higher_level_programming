40f56cfbbf7dc8a10af122e4f63ebfaedbda2dbd
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-01-09 07:14:13 +0000

    debugging task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
 
     argv = sys.argv[1:]
-    print("{:d} arguments".format(len(argv)))
+    print("{:d} arguments:".format(len(argv)))
     count = 1
     for arg in argv:
         print("{:d}: {:s}".format(count, arg))
